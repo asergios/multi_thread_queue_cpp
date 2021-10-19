7c7f041fd3cd19be62b1387af0d2026b51a58b31
António Silva <asergiosilv@gmail.com>
2021-10-19 21:59:25 +0100

    Initial Commit

---
+++ Queue.h
@@ -0,0 +1,30 @@
+
+template <typename T>
+class Queue
+{
+public:
+	Queue(int size)
+	{
+
+	}
+
+	void Push(T element)
+	{
+
+	}
+
+	T Pop()
+	{
+		
+	}
+
+	int Count()
+	{
+
+	}
+
+	int Size()
+	{
+
+	}
+};
