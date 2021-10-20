311235f52bc118cdf332578e333f5290e946f445
António Silva <asergiosilv@gmail.com>
2021-10-20 21:37:36 +0100

    Adds initial proposal of Mutex in Queue * Adds race condition protection with Mutex "queue_mutex"; * Adds condition_variable "block" to block thread while no operation is possible;

--- Queue.h
+++ Queue.h
@@ -1,13 +1,17 @@
+#include <mutex>
+#include <condition_variable>
 
 template <typename T>
 class Queue
 {
 private:
-	int *array;		// Pointer for array where elements will be stored
-	int capacity;	// Total size of the Queue
-	int count;		// Current total of elements in the queue
-	int push_index; // Current index to where next element shall be added 
-	int pop_index;  // Current index for where next element shall be removed (first in)
+	int *array;						// Pointer for array where elements will be stored
+	int capacity;					// Total size of the Queue
+	int count;						// Current total of elements in the queue
+	int push_index; 				// Current index to where next element shall be added 
+	int pop_index;  				// Current index for where next element shall be removed (first in)
+	std::mutex queue_mutex; 		// Protects against race conditions of shared variables
+	std::condition_variable block; 	// Block thread while it waits for new data to be added or removed
 
 public:
 	// Call constructor
@@ -23,21 +27,37 @@ public:
 	// Adds one element to the end of the queue;
 	void Push(T element)
 	{
-		if(count == capacity) return;	// Implement Mutex Here
+		std::unique_lock<std::mutex> lock(queue_mutex);	// Locks the Mutex
+		while(count == capacity)						// Queue is at full capacity, wait for a element to be removed
+		{
+			block.wait(lock);							// Release the Mutex and suspend the execution of the thread
+		}
 
-		array[push_index] = element;
-		push_index = (push_index + 1) % capacity;	// Values between 0 - (capacity-1)
-		count++;
+		array[push_index] = element;					// Adds the new element
+		push_index = (push_index + 1) % capacity;		// Values between 0 - (capacity-1)
+		++count;										// Updates total elements in Queue
+
+		lock.unlock();									// Unlock Mutex before notifying to prevent other thread from waking up
+	 	 												// 	just to be blocked again afterwards
+		block.notify_one();								// Notify one thread that might be waiting
 	}
 
 	// Removes one element from the queue;
 	T Pop()
 	{
-		if(count == 0) return 0;			// Implement Mutex Here
+		std::unique_lock<std::mutex> lock(queue_mutex);	// Locks the Mutex
+		while(count == 0)								// Queue empty, wait for a element to be added
+		{
+			block.wait(lock);							// Release the Mutex and suspend the execution of the thread
+		}
 
-		pop_index = (pop_index + 1) % capacity;	    // Values between 0 - (capacity-1)
-		count--;
-		return array[pop_index];
+		pop_index = (pop_index + 1) % capacity;	    	// Values between 0 - (capacity-1)
+		--count;										// Updates total elements in Queue
+
+		lock.unlock();									// Unlock Mutex before notifying to prevent other thread from waking up
+	 	 												// 	just to be blocked again afterwards
+		block.notify_one();								// Notify one thread that might be waiting
+		return array[pop_index];						// Return removed element
 	}
 
 	// Returns the current number of elements in the queue;
