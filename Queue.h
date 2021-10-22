348a70720520eb8731273c9ed6ed9ee620dab8c7
António Silva <asergiosilv@gmail.com>
2021-10-22 20:37:59 +0100

    Changes Mutex and condition variable to be controlled in the threads * In order for the prints to also be sync, these need to be executed when mutex is locked;

--- Queue.h
+++ Queue.h
@@ -10,10 +10,11 @@ private:
 	int count;						// Current total of elements in the queue
 	int push_index; 				// Current index to where next element shall be added 
 	int pop_index;  				// Current index for where next element shall be removed (first in)
-	std::mutex queue_mutex; 		// Protects against race conditions of shared variables
-	std::condition_variable block; 	// Block thread while it waits for new data to be added or removed
 
 public:
+	std::mutex queue_mutex; 		// Protects against race conditions of shared variables
+	std::condition_variable block; 	// Block thread while it waits for new data to be added or removed
+	
 	// Call constructor
 	Queue(int size)
 	{
@@ -27,36 +28,16 @@ public:
 	// Adds one element to the end of the queue;
 	void Push(T element)
 	{
-		std::unique_lock<std::mutex> lock(queue_mutex);	// Locks the Mutex
-		while(count == capacity)						// Queue is at full capacity, wait for a element to be removed
-		{
-			block.wait(lock);							// Release the Mutex and suspend the execution of the thread
-		}
-
 		array[push_index] = element;					// Adds the new element
 		push_index = (push_index + 1) % capacity;		// Values between 0 - (capacity-1)
 		++count;										// Updates total elements in Queue
-
-		lock.unlock();									// Unlock Mutex before notifying to prevent other thread from waking up
-	 	 												// 	just to be blocked again afterwards
-		block.notify_one();								// Notify one thread that might be waiting
 	}
 
 	// Removes one element from the queue;
 	T Pop()
 	{
-		std::unique_lock<std::mutex> lock(queue_mutex);	// Locks the Mutex
-		while(count == 0)								// Queue empty, wait for a element to be added
-		{
-			block.wait(lock);							// Release the Mutex and suspend the execution of the thread
-		}
-
 		pop_index = (pop_index + 1) % capacity;	    	// Values between 0 - (capacity-1)
 		--count;										// Updates total elements in Queue
-
-		lock.unlock();									// Unlock Mutex before notifying to prevent other thread from waking up
-	 	 												// 	just to be blocked again afterwards
-		block.notify_one();								// Notify one thread that might be waiting
 		return array[pop_index];						// Return removed element
 	}
 
--- main.cpp
+++ main.cpp
@@ -1,22 +1,59 @@
 #include<iostream>
+#include<thread>
 #include "Queue.h"
 using namespace std;
+
+void push_thread(Queue<int>* queue)
+{
+	while(1)
+	{
+		std::unique_lock<std::mutex> lock(queue->queue_mutex);	// Locks the Mutex
+		while(queue->Count() == queue->Size())					// Queue is at full capacity, wait for a element to be removed
+		{
+			queue->block.wait(lock);							// Release the Mutex and suspend the execution of the thread
+		}
+
+		int random = rand() % 100 + 1;
+		queue->Push(random);
+		cout << "Push(" + to_string(random) + ")\n";	
+		//this_thread::sleep_for(50ms);
+
+		lock.unlock();											// Unlock Mutex before notifying to prevent other thread from waking up
+	 	 														// 	just to be blocked again afterwards
+		queue->block.notify_one();								// Notify one thread that might be waiting
+	}
+}
+
+void pop_thread(Queue<int>* queue)
+{
+	while(1)
+	{
+		std::unique_lock<std::mutex> lock(queue->queue_mutex);	// Locks the Mutex
+		while(queue->Count() == 0)								// Queue empty, wait for a element to be added
+		{
+			queue->block.wait(lock);							// Release the Mutex and suspend the execution of the thread
+		}
+
+		cout << "Removed element:" + to_string(queue->Pop()) + "\n";	
+		//this_thread::sleep_for(100ms);
+
+		lock.unlock();											// Unlock Mutex before notifying to prevent other thread from waking up
+	 	 														// 	just to be blocked again afterwards
+		queue->block.notify_one();								// Notify one thread that might be waiting
+	}
+}
 
 int main()
 {
 
-	Queue<int> sample(5);
+	cout << "Writing Thread 		Queue Reading 		Thread\n";	
+	Queue<int> sample(2);
+	cout << "New Queue<int>(2)\n";	
+	thread t1(push_thread, &sample);
+	thread t2(pop_thread, &sample);
 
-	// Fill queue
-	sample.Push(1);
-	sample.Push(2);
-	sample.Push(3);
-	// Pop from queue
-	cout << sample.Pop();
-	cout << sample.Pop();
-	cout << sample.Pop();
-	cout << sample.Pop();
-	cout << sample.Pop();
-
+	t1.join();
+	t2.join();
+	
     return 0;
 }
